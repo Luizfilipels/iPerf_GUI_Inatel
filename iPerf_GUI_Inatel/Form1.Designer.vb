902fd5104a7c0701204b5ce6e88fafcffa305d49
Luiz Filipe Silva Lopes <luizfilipesl@hotmail.com>
2024-12-07 23:26:57 -0300

    Design inicial

--- iPerf_GUI_Inatel/Form1.Designer.vb
+++ iPerf_GUI_Inatel/Form1.Designer.vb
@@ -22,10 +22,168 @@ Partial Class Form1
     'Não o modifique usando o editor de códigos.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txtIP = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPort = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTime = New System.Windows.Forms.TextBox()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.txtOutput = New System.Windows.Forms.RichTextBox()
+        Me.chkClient = New System.Windows.Forms.CheckBox()
+        Me.chkServer = New System.Windows.Forms.CheckBox()
+        Me.SuspendLayout()
+        '
+        'txtIP
+        '
+        Me.txtIP.Location = New System.Drawing.Point(57, 90)
+        Me.txtIP.Name = "txtIP"
+        Me.txtIP.Size = New System.Drawing.Size(214, 20)
+        Me.txtIP.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(110, 29)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "iPerf GUI"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(19, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(33, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "IP -"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(19, 133)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(56, 20)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Porta -"
+        '
+        'txtPort
+        '
+        Me.txtPort.Location = New System.Drawing.Point(82, 132)
+        Me.txtPort.Name = "txtPort"
+        Me.txtPort.Size = New System.Drawing.Size(189, 20)
+        Me.txtPort.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(19, 172)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(145, 20)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Duração do Teste -"
+        '
+        'txtTime
+        '
+        Me.txtTime.Location = New System.Drawing.Point(171, 171)
+        Me.txtTime.Name = "txtTime"
+        Me.txtTime.Size = New System.Drawing.Size(100, 20)
+        Me.txtTime.TabIndex = 6
+        '
+        'btnStart
+        '
+        Me.btnStart.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStart.Location = New System.Drawing.Point(23, 214)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(102, 23)
+        Me.btnStart.TabIndex = 7
+        Me.btnStart.Text = "Iniciar Teste"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStop.Location = New System.Drawing.Point(23, 243)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(102, 23)
+        Me.btnStop.TabIndex = 8
+        Me.btnStop.Text = "Parar Teste"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(277, 9)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.ReadOnly = True
+        Me.txtOutput.Size = New System.Drawing.Size(511, 429)
+        Me.txtOutput.TabIndex = 9
+        Me.txtOutput.Text = ""
+        '
+        'chkClient
+        '
+        Me.chkClient.AutoSize = True
+        Me.chkClient.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkClient.Location = New System.Drawing.Point(171, 216)
+        Me.chkClient.Name = "chkClient"
+        Me.chkClient.Size = New System.Drawing.Size(67, 20)
+        Me.chkClient.TabIndex = 10
+        Me.chkClient.Text = "Cliente"
+        Me.chkClient.UseVisualStyleBackColor = True
+        '
+        'chkServer
+        '
+        Me.chkServer.AutoSize = True
+        Me.chkServer.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkServer.Location = New System.Drawing.Point(171, 243)
+        Me.chkServer.Name = "chkServer"
+        Me.chkServer.Size = New System.Drawing.Size(77, 20)
+        Me.chkServer.TabIndex = 11
+        Me.chkServer.Text = "Servidor"
+        Me.chkServer.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Controls.Add(Me.chkServer)
+        Me.Controls.Add(Me.chkClient)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.txtTime)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtPort)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtIP)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "iPerf GUI - Inatel"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txtIP As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPort As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTime As TextBox
+    Friend WithEvents btnStart As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents txtOutput As RichTextBox
+    Friend WithEvents chkClient As CheckBox
+    Friend WithEvents chkServer As CheckBox
 End Class
--- iPerf_GUI_Inatel/Form1.vb
+++ iPerf_GUI_Inatel/Form1.vb
@@ -1,3 +1,71 @@
-﻿Public Class Form1
+﻿Imports System.Diagnostics
+Imports System.IO
 
+Public Class Form1
+    Private iperfProcess As Process
+
+    ' Botão "Iniciar"
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        ' Verifica se cliente e servidor estão ambos selecionados ou ambos desmarcados
+        If chkClient.Checked = chkServer.Checked Then
+            MessageBox.Show("Selecione apenas Cliente ou Servidor.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Configurar argumentos do iPerf
+        Dim arguments As String
+        If chkServer.Checked Then
+            arguments = "-s"
+        Else
+            Dim ip As String = txtIP.Text
+            Dim port As String = txtPort.Text
+            Dim time As String = txtTime.Text
+            arguments = $"-c {ip} -p {port} -t {time}"
+        End If
+
+        ' Caminho para o iPerf
+        Dim iperfPath As String = ".\Bin\iperf3.exe"
+        If Not File.Exists(iperfPath) Then
+            MessageBox.Show($"O arquivo iPerf não foi encontrado no caminho: {iperfPath}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return
+        End If
+
+        ' Configurar e iniciar o processo do iPerf
+        Try
+            iperfProcess = New Process()
+            iperfProcess.StartInfo.FileName = iperfPath
+            iperfProcess.StartInfo.Arguments = arguments
+            iperfProcess.StartInfo.RedirectStandardOutput = True
+            iperfProcess.StartInfo.UseShellExecute = False
+            iperfProcess.StartInfo.CreateNoWindow = True
+
+            ' Capturar saída do processo
+            AddHandler iperfProcess.OutputDataReceived, AddressOf OnOutputDataReceived
+
+            iperfProcess.Start()
+            iperfProcess.BeginOutputReadLine()
+
+            txtOutput.AppendText("Teste iniciado..." & Environment.NewLine)
+        Catch ex As Exception
+            MessageBox.Show($"Erro ao iniciar o iPerf: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ' Botão "Parar"
+    Private Sub btnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        If iperfProcess IsNot Nothing AndAlso Not iperfProcess.HasExited Then
+            iperfProcess.Kill()
+            txtOutput.AppendText("Teste parado." & Environment.NewLine)
+        End If
+    End Sub
+
+    ' Capturar saída do iPerf
+    Private Sub OnOutputDataReceived(sender As Object, e As DataReceivedEventArgs)
+        If e.Data IsNot Nothing Then
+            Me.Invoke(Sub()
+                          txtOutput.AppendText(e.Data & Environment.NewLine)
+                      End Sub)
+        End If
+    End Sub
 End Class
+
